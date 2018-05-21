9505b32f0d70b340504c76c6b82fbee3700b4bfc
prusswan <prusswan@gmail.com>
2018-05-22 02:07:53 +0800

    fixed selection issue with multiple divs

--- app/controllers/d3_controller.rb
+++ app/controllers/d3_controller.rb
@@ -4,8 +4,11 @@ class D3Controller < ApplicationController
   def index
     respond_to do |format|
       format.html do
-        render(template: "#{params[:figure]}.html")
-          .gsub!('d3.v3.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.js')
+        result = render(template: "#{params[:figure]}.html")
+        result = result.gsub!('d3.v3.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.js') || result
+        result = result.gsub!('.select("body")', '.select(".col")') || result
+
+        return result
       end
     end
   end
