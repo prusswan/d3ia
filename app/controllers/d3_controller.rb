16d0cfaeddb01605a3142d0b890c2e643a756fb9
prusswan <prusswan@gmail.com>
2018-05-21 16:34:16 +0800

    added example list menu and fixed d3 links

--- app/controllers/d3_controller.rb
+++ app/controllers/d3_controller.rb
@@ -2,6 +2,11 @@ class D3Controller < ApplicationController
   prepend_view_path Rails.root
 
   def index
-    render template: "#{params[:figure]}.html"
+    respond_to do |format|
+      format.html do
+        render(template: "#{params[:figure]}.html")
+          .gsub!('d3.v3.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.js')
+      end
+    end
   end
 end
