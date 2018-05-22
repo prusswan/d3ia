7daae81a7aa8edde34a57982956105d6523062b7
prusswan <prusswan@gmail.com>
2018-05-22 15:21:52 +0800

    added option to show raw html example

--- app/controllers/d3_controller.rb
+++ app/controllers/d3_controller.rb
@@ -2,9 +2,10 @@ class D3Controller < ApplicationController
   prepend_view_path Rails.root
 
   def index
+
     respond_to do |format|
       format.html do
-        result = render(template: "#{params[:figure]}.html")
+        result = render(template: "#{params[:figure]}.html", layout: !(false || params[:raw]))
         result = result.gsub!('d3.v3.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.js') || result
         result = result.gsub!('.select("body")', '.select(".col")') || result
 
